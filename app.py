3f898453ec4a1406af27766d0be7bfec35873d57
Shreyas Lagvankar <shreyaslagvankar@gmail.com>
2025-12-11 21:07:25 +0530

    Use Render PORT env variable

--- app.py
+++ app.py
@@ -58,5 +58,9 @@ def home():
     return "Flood Prediction Cloud API Running"
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
